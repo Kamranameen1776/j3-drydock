f81c637d81533e2a2413a83a11dc9e6e8e8bf7d4
Ivan Chensky <Ivan.Chenskyi@jibe.solutions>
2023-10-05 08:22:29 -0700

    add fe lilters

--- frontend/projects/j3-drydock/src/lib/drydock/components/projects-main-page/projects-specifications-grid/ProjectsSpecificationGridService.ts
+++ frontend/projects/j3-drydock/src/lib/drydock/components/projects-main-page/projects-specifications-grid/ProjectsSpecificationGridService.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Column, GridButton, eColor } from 'jibe-components';
+import { Column, Filter, FilterListSet, GridButton, eColor, eFieldControlType } from 'jibe-components';
 import { nameOf } from '../../../common/ts-helpers/nameOf';
 import { GridInputsWithRequest } from '../../../presentation-layer/jb-components-helpers/grid-inputs';
 import { ProjectsForMainPageGridDto } from './bll/dtos/ProjectsForMainPageGridDto';
 import { ProjectsService } from '../../../infrastructure-layer/api-services/projects/ProjectsService';
+import { of } from 'rxjs';
 
 @Injectable()
 export class ProjectsSpecificationGridService {
+  // TODO: get from the backend service
+  public readonly minDate: Date = new Date('2010-01-01');
+  public readonly maxDate: Date = new Date('2100-01-01');
+
   public readonly gridName: string = 'projectsSpecificationGrid';
+
+  initDate: Date = new Date();
 
   private readonly columns: Column[] = [
     {
@@ -129,14 +136,114 @@ export class ProjectsSpecificationGridService {
     show: true
   };
 
+  public filters: Filter[] = [
+    {
+      Active_Status: true,
+      Active_Status_Config_Filter: true,
+      ControlType: eFieldControlType.MultiSelect,
+      Created_By: null,
+      DataType: null,
+      DisplayText: 'Project Type',
+      FieldName: 'ProjectTypes',
+      FieldID: 0,
+      default: true,
+      selectedValues: null,
+      CoupleID: 0,
+      CoupleLabel: 'Project',
+      gridName: this.gridName,
+      list: of(this.LoadProjectTypes())
+    },
+    {
+      Active_Status: true,
+      Active_Status_Config_Filter: true,
+      ControlType: eFieldControlType.MultiSelect,
+      Created_By: null,
+      DataType: null,
+      DisplayText: 'Project Manager',
+      FieldName: 'ProjectManagers',
+      FieldID: 0,
+      default: true,
+      selectedValues: null,
+      CoupleID: 0,
+      CoupleLabel: 'Project',
+      gridName: this.gridName,
+      list: of(this.LoadProjectsManagers())
+    },
+    {
+      Active_Status: true,
+      Active_Status_Config_Filter: true,
+      ControlType: eFieldControlType.Date,
+      Created_By: null,
+      DataType: null,
+      DisplayText: 'Start Date',
+      FieldName: 'StartDate',
+      FieldID: 1,
+      default: true,
+      selectedValues: new Date(this.initDate.getFullYear(), this.initDate.getMonth(), 1),
+      minDate: this.minDate,
+      maxDate: this.maxDate,
+      CoupleID: 1,
+      CoupleLabel: 'Last Verification Date',
+      gridName: this.gridName
+    },
+    {
+      Active_Status: true,
+      Active_Status_Config_Filter: true,
+      ControlType: eFieldControlType.Date,
+      Created_By: null,
+      DataType: null,
+      DisplayText: 'End Date',
+      FieldName: 'EndDate',
+      FieldID: 2,
+      default: true,
+      selectedValues: new Date(this.initDate.getFullYear(), this.initDate.getMonth() + 1, 0),
+      minDate: this.minDate,
+      maxDate: this.maxDate,
+      CoupleID: 1,
+      CoupleLabel: 'Last Verification Date',
+      gridName: this.gridName
+    }
+  ];
+
+  private filterListsSet: FilterListSet = {
+    ProjectTypes: {
+      type: eFieldControlType.MultiSelect,
+      odataKey: ''
+    },
+    ProjectsManages: {
+      type: eFieldControlType.MultiSelect,
+      odataKey: ''
+    },
+    StartDate: {
+      type: eFieldControlType.Date,
+      odataKey: '',
+      dateMethod: 'ge'
+    },
+    EndDate: {
+      type: eFieldControlType.Date,
+      odataKey: '',
+      dateMethod: 'le'
+    }
+  };
+
   constructor(private projectsService: ProjectsService) {}
 
   public getGridInputs(): GridInputsWithRequest {
     return {
       columns: this.columns,
       gridName: this.gridName,
+      filters: this.filters,
+      filtersLists: this.filterListsSet,
       request: this.projectsService.getProjectsForMainPageGridRequest(),
       gridButton: this.gridButton
     };
+  }
+
+  private LoadProjectTypes(): string[] {
+    return ['Dry Dock', 'b'];
+  }
+
+  private LoadProjectsManagers(): string[] {
+    return ['a', 'b'];
   }
 }
