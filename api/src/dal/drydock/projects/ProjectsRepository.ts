427d618d4396abc2e3fffab49136fcbc5b3bb538
Ivan Chensky <Ivan.Chenskyi@jibe.solutions>
2023-10-11 05:49:25 -0700

    adopt to new project schema(added project state table)

--- api/src/dal/drydock/projects/ProjectsRepository.ts
+++ api/src/dal/drydock/projects/ProjectsRepository.ts
@@ -10,20 +10,20 @@ export class ProjectsRepository {
     public async GetProjectStatuses(): Promise<ProjectStatusResultDto[]> {
         const result = await getManager().query(
             `
-SELECT distinct wdetails.[WorkflowType_ID] as ProjectStatusId,
-	wdetails.[status_display_name] as ProjectStatusName,
-	wdetails.Workflow_OrderID
-	
-FROM JMS_DTL_Workflow_config_Details as wdetails
-
-INNER JOIN TEC_LIB_Worklist_Type as wt on wdetails.Config_ID = wt.ID
-INNER JOIN [dry_dock].[project_type] as pt on pt.[project_type_code] = wt.Worklist_Type
-
-WHERE wt.Active_Status=1
-	AND wdetails.Active_Status=1
-	AND pt.[date_of_deletion] IS NULL
-
-ORDER BY wdetails.Workflow_OrderID
+            SELECT distinct wdetails.[WorkflowType_ID] as ProjectStatusId,
+            wdetails.[status_display_name] as ProjectStatusName,
+            wdetails.Workflow_OrderID
+            
+        FROM JMS_DTL_Workflow_config_Details as wdetails
+        
+        INNER JOIN TEC_LIB_Worklist_Type as wt on wdetails.Config_ID = wt.ID
+        INNER JOIN [dry_dock].[project_type] as pt on pt.[TEC_LIB_Worklist_Type] = wt.Worklist_Type
+        
+        WHERE wt.Active_Status=1
+            AND wdetails.Active_Status=1
+            AND pt.[date_of_deletion] IS NULL
+        
+        ORDER BY wdetails.Workflow_OrderID
         
             `,
         );
@@ -34,12 +34,12 @@ ORDER BY wdetails.Workflow_OrderID
     public async GetProjectTypes(): Promise<ProjectTypeResultDto[]> {
         const result = await getManager().query(
             `
-            SELECT pt.[project_type_code] AS 'ProjectTypeCode' 
+            SELECT pt.[TEC_LIB_Worklist_Type] AS 'ProjectTypeCode' 
             ,wt.[Worklist_Type_Display] AS 'ProjectTypeName'
 			,pt.[short_code] as 'ProjectTypeShortCode'
         FROM [dry_dock].[project_type] as pt
 
-		INNER JOIN TEC_LIB_Worklist_Type as wt on pt.[project_type_code] = wt.Worklist_Type
+		INNER JOIN TEC_LIB_Worklist_Type as wt on pt.[TEC_LIB_Worklist_Type] = wt.Worklist_Type
 
         WHERE pt.[date_of_deletion] IS NULL
             `,
@@ -51,12 +51,13 @@ ORDER BY wdetails.Workflow_OrderID
     public async GetProjectStates(): Promise<any[]> {
         const result = await getManager().query(
             `
-        --select [data]
-        --from j2_inf_admin_library as al
-
-        --
-        --                      TODO: pass value from project_type table 
-        --where al.[library_code] = 'tmDdStates'
+            SELECT [project_state_id]
+            ,[project_state_name]
+        FROM [JIBE_Main].[dry_dock].[project_state]
+      
+        -- TODO: pass the project type id as a parameter
+        WHERE [project_type_id] = 1 
+        AND [date_of_deletion] IS NULL
             `,
         );
 
@@ -73,7 +74,7 @@ ORDER BY wdetails.Workflow_OrderID
             ,pr.[project_code] AS 'ProjectCode'
             ,vessel.[Vessel_Name] AS 'VesselName'
 			,wt.[Worklist_Type_Display] as ProjectTypeName
-            ,pr.[project_state_name] AS 'ProjectStateName'
+            ,ps.[project_state_name] AS 'ProjectStateName'
             ,pr.[subject] AS 'Subject'
             ,usr.[First_Name] + ' ' + usr.[Last_Name] AS 'ProjectManager'
             ,cast(pr.[start_date] as datetimeoffset) AS 'StartDate'
@@ -82,7 +83,11 @@ ORDER BY wdetails.Workflow_OrderID
 
         INNER JOIN [Lib_Vessels] as vessel ON [Lib_Vessels_Vessel_ID] = vessel.[Vessel_ID]
         INNER JOIN [Lib_User] as usr ON [project_manager_Lib_User_Uid] = usr.[uid]
-		INNER JOIN TEC_LIB_Worklist_Type as wt on pr.[project_type_code] = wt.Worklist_Type
+		INNER JOIN [dry_dock].[project_type] as pt ON pt.[project_type_id] = pr.[project_type_id]
+		INNER JOIN TEC_LIB_Worklist_Type as wt on pt.[TEC_LIB_Worklist_Type] = wt.Worklist_Type
+		INNER JOIN [dry_dock].[project_state] as ps ON ps.[project_state_id] = pr.[project_state_id] 
+			and pt.[project_type_id] = ps.[project_type_id]
+		
 
         WHERE pr.[date_of_deletion] IS NULL
             `,
