f3f58694208a50fb1a8611f967135c7d014a36a1
Anatolii.Kutovyi@jibe.solutions <Anatolii.Kutovyi@jibe.solutions>
2023-11-16 09:49:46 +0200

    test git rights

--- api/src/controllers/drydock/specification-details/delete-specification-details.ts
+++ api/src/controllers/drydock/specification-details/delete-specification-details.ts
@@ -9,7 +9,7 @@ async function deleteSpecificationDetails(req: Request, res: Response) {
     await middlewareHandler.ExecuteAsync(req, res, async (request: Request) => {
         const command = new DeleteSpecificationDetailsCommand();
 
-        return await command.ExecuteAsync(request.body);
+        return command.ExecuteAsync(request.body);
     });
 }
 
