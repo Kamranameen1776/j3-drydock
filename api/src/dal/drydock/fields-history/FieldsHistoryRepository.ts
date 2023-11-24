eea1c4237cff70683dce0d44f9e139ecd59f47e7
Anton <anton@JB-UKN-LPTP026.local>
2023-11-24 14:56:36 +0200

    fix: slow request due to typeorm middleware

--- api/src/dal/drydock/fields-history/FieldsHistoryRepository.ts
+++ api/src/dal/drydock/fields-history/FieldsHistoryRepository.ts
@@ -1,6 +1,4 @@
-import { QueryRunner } from 'typeorm';
-
-import { J2FieldsHistoryEntity } from '../../../entity/drydock/dbo/J2FieldsHistoryEntity';
+import { getConnection, QueryRunner } from 'typeorm';
 
 export type CreateFieldsHistoryDto = {
     key1: string;
@@ -17,19 +15,43 @@ export type CreateFieldsHistoryDto = {
     actionName: string;
     createdDate: Date;
     createdBy: string;
-}
+};
+
 export class FieldsHistoryRepository {
-    public async saveFieldsHistory(
-        fieldsHistory: CreateFieldsHistoryDto,
-        queryRunner: QueryRunner,
-    ): Promise<J2FieldsHistoryEntity> {
-        const fieldsHistoryRepository = queryRunner.manager.getRepository(J2FieldsHistoryEntity);
-        const data = await fieldsHistoryRepository.save(fieldsHistory);
-        return data;
+    public async saveFieldsHistory(fieldsHistory: CreateFieldsHistoryDto, queryRunner: QueryRunner): Promise<void> {
+        await this.insertQuery(fieldsHistory, queryRunner);
     }
 
     public async insertMany(fieldsHistories: CreateFieldsHistoryDto[], queryRunner: QueryRunner): Promise<void> {
-        const fieldsHistoryRepository = queryRunner.manager.getRepository(J2FieldsHistoryEntity);
-        await fieldsHistoryRepository.insert(fieldsHistories);
+        for (const fieldsHistory of fieldsHistories) {
+            await this.insertQuery(fieldsHistory, queryRunner);
+        }
+    }
+
+    private async insertQuery(fieldsHistory: CreateFieldsHistoryDto, queryRunner: QueryRunner): Promise<void> {
+        const sql = `
+            INSERT INTO j2_fields_history
+            (key_1, key_2, key_3, module_code, function_code, is_current, version_number, table_name, section,
+             display_text, value, action_name, created_date, created_by)
+            VALUES
+            (@0, @1, @2, @3, @4, @5, @6, @7, @8, @9, @10, @11, @12, @13)
+        `;
+
+        await queryRunner.manager.query(sql, [
+            fieldsHistory.key1,
+            fieldsHistory.key2,
+            fieldsHistory.key3,
+            fieldsHistory.moduleCode,
+            fieldsHistory.functionCode,
+            fieldsHistory.isCurrent,
+            fieldsHistory.versionNumber,
+            fieldsHistory.tableName,
+            fieldsHistory.section,
+            fieldsHistory.displayText,
+            fieldsHistory.value,
+            fieldsHistory.actionName,
+            fieldsHistory.createdDate,
+            fieldsHistory.createdBy,
+        ]);
     }
 }
