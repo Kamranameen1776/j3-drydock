fbee992f9ed328f86220c0137457fa4c1d83a814
Anton.Bilchuck <Anton.Bilchuck@jibe.solutions>
2023-11-27 11:33:08 +0000

    Merged PR 82647: feat: add model for audit trail, service and integrate into commands
    ## Description
    
    [XXXXXX](https://dev.azure.com/jibe-erp/JiBe/_workitems/edit/XXXXXX)
    
    Please give a description of the feature including relevant screen shots and links to Azure workitems.
    
    ## Testing
    
    Please give a description of any manual testing steps that might be required.
    
    ## Breaking changes
    
    Does this PR cause breaking changes that might impact other developers, please describe them here and what other
    developers might have to accomodate for.
    
    ## Checklist
    <!---
    Please remove any checklist items not relevant to your PR
    -->
    
    - [ ] This PR fulfills all acceptance criteria
    - [ ] Relevant well written tests included
    
    feat: add model for audit trail, service and integrate into commands
    
    Related work items: #685686

---
+++ api/src/bll/drydock/specification-details/specification-details-audit.service.ts
@@ -0,0 +1,76 @@
+import { QueryRunner } from 'typeorm';
+
+import { UpdateSpecificationDetailsDto } from '../../../application-layer/drydock/specification-details/dtos/UpdateSpecificationDetailsDto';
+import {
+    CreateFieldsHistoryDto,
+    FieldsHistoryRepository,
+} from '../../../dal/drydock/fields-history/FieldsHistoryRepository';
+import { TaskManagerConstants } from '../../../shared/constants/task-manager';
+
+export class SpecificationDetailsAuditService {
+    private readonly fieldsHistoryRepository = new FieldsHistoryRepository();
+
+    private generateCommonFields(uid: string): Partial<CreateFieldsHistoryDto> {
+        return {
+            key1: uid,
+            key2: '0',
+            key3: '',
+            moduleCode: TaskManagerConstants.project.module_code,
+            functionCode: TaskManagerConstants.project.function_code,
+            isCurrent: true,
+            versionNumber: 1,
+            tableName: 'specification_details',
+            section: 'Header Section',
+        };
+    }
+
+    public async auditCreatedSpecificationDetails(
+        specificationDetail: UpdateSpecificationDetailsDto,
+        createdById: string,
+        queryRunner: QueryRunner,
+    ): Promise<void> {
+        const now = new Date();
+        const fields = Object.entries(specificationDetail).map(([key, value]) => ({
+            ...this.generateCommonFields(specificationDetail.uid),
+            displayText: key,
+            value: value,
+            actionName: 'Created',
+            createdDate: now,
+            createdBy: createdById,
+        }));
+
+        await this.fieldsHistoryRepository.insertMany(fields as CreateFieldsHistoryDto[], queryRunner);
+    }
+
+    public async auditDeletedSpecificationDetails(
+        uid: string,
+        deletedById: string,
+        queryRunner: QueryRunner,
+    ): Promise<void> {
+        const deleteField = {
+            ...this.generateCommonFields(uid),
+            actionName: 'Deleted',
+            createdBy: deletedById,
+        };
+
+        await this.fieldsHistoryRepository.saveFieldsHistory(deleteField as CreateFieldsHistoryDto, queryRunner);
+    }
+
+    public async auditUpdatedSpecificationDetails(
+        specificationDetail: UpdateSpecificationDetailsDto,
+        updatedById: string,
+        queryRunner: QueryRunner,
+    ): Promise<void> {
+        const now = new Date();
+        const fields = Object.entries(specificationDetail).map(([key, value]) => ({
+            ...this.generateCommonFields(specificationDetail.uid),
+            displayText: key,
+            value: value,
+            actionName: 'Amended',
+            createdDate: now,
+            createdBy: updatedById,
+        }));
+
+        await this.fieldsHistoryRepository.insertMany(fields as CreateFieldsHistoryDto[], queryRunner);
+    }
+}
