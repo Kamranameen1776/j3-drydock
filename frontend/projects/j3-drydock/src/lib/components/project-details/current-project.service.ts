7d91fc510784bd91632215c5df82c41a85d497ad
Prokopenko.Sergii <Prokopenko.Sergii@jibe.solutions>
2023-11-29 13:22:11 +0700

    small fix

--- frontend/projects/j3-drydock/src/lib/components/project-details/current-project.service.ts
+++ frontend/projects/j3-drydock/src/lib/components/project-details/current-project.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { TaskManagerService } from '../../services/task-manager.service';
 
 @Injectable()
 export class CurrentProjectService {
   projectId$ = new BehaviorSubject<string>(null);
   vesselUid$ = new BehaviorSubject<string>(null);
-
-  constructor(private taskManagerService: TaskManagerService) {}
 }
