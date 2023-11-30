15d4dca6837ebe9972500184184c90b8d8fed6b4
Prokopenko.Sergii <Prokopenko.Sergii@jibe.solutions>
2023-11-30 12:12:02 +0700

    working on feed-discusion status change

--- frontend/projects/j3-drydock/src/lib/services/details.service.ts
+++ frontend/projects/j3-drydock/src/lib/services/details.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
 import { ApiRequestService, UserService, eAppLocation } from 'jibe-components';
+import { TaskManagerService } from './task-manager.service';
+import { SaveWorklowChangeToDiscussionFeed } from '../models/interfaces/task-manager';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DetailsService {
-  constructor(private apiRequestService: ApiRequestService) {}
+  constructor(
+    private apiRequestService: ApiRequestService,
+    private taskManagerService: TaskManagerService
+  ) {}
 
   isOffice(): 1 | 0 {
     return UserService.getUserDetails().AppLocation === eAppLocation.Office ? 1 : 0;
@@ -18,5 +23,24 @@ export class DetailsService {
       key3: '0',
       uid: uid
     };
+  }
+
+  sendStatusChangeToWorkflowAndFollow(val: SaveWorklowChangeToDiscussionFeed) {
+    const saveEventsObj: Record<string, unknown> = {
+      module_code: val.module,
+      function_code: val.function,
+      key1: val.uid,
+      key2: String(this.isOffice()),
+      key3: '0',
+      details: JSON.stringify({
+        wl_type: val.wlType,
+        job_card_no: val.uid,
+        status: val.statusCode
+      }),
+      remark: val.remark,
+      action_code: val.statusName
+    };
+
+    return this.taskManagerService.saveWorkFlowAndFollow(saveEventsObj);
   }
 }
