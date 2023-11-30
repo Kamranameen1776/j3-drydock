5824fa4004a9045257403e17ec12b808a0291631
Abramov Oleksii <abramov.oleksii@jibe.solutions>
2023-11-30 12:02:23 +0200

    Fixes

--- api/src/application-layer/drydock/standard-jobs/GetStandardJobPopupQuery.ts
+++ api/src/application-layer/drydock/standard-jobs/GetStandardJobPopupQuery.ts
@@ -1,13 +1,15 @@
 import { StandardJobsRepository } from '../../../dal/drydock/standard-jobs/StandardJobsRepository';
-import { ODataRequestDto } from '../../../shared/dto';
 import { ODataResult } from '../../../shared/interfaces';
 import { Query } from '../core/cqrs/Query';
-import { GetStandardJobPopupDto } from './dto';
+import { GetStandardJobPopupDto, GetStandardJobPopupRequestDto } from './dto';
 
-export class GetStandardJobPopupQuery extends Query<ODataRequestDto, ODataResult<GetStandardJobPopupDto>> {
+export class GetStandardJobPopupQuery extends Query<
+    GetStandardJobPopupRequestDto,
+    ODataResult<GetStandardJobPopupDto>
+> {
     standardJobRepository = new StandardJobsRepository();
 
-    protected async MainHandlerAsync(request: ODataRequestDto) {
+    protected async MainHandlerAsync(request: GetStandardJobPopupRequestDto) {
         return this.standardJobRepository.getStandardJobsPopupData(request);
     }
 }
