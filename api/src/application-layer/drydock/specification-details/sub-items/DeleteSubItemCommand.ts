412b14c5ac3e0b6bc9f6ac0fbbd0a18f33a18168
tolia <anatolii.kutovyi@jibe.solutions>
2023-11-30 10:12:10 +0200

    add syncing spec-sub-items

--- api/src/application-layer/drydock/specification-details/sub-items/DeleteSubItemCommand.ts
+++ api/src/application-layer/drydock/specification-details/sub-items/DeleteSubItemCommand.ts
@@ -1,12 +1,17 @@
+import { SynchronizerService } from 'j2utils';
+
 import { validateAgainstModel } from '../../../../common/drydock/ts-helpers/validate-against-model';
 import { DeleteOneParams } from '../../../../dal/drydock/specification-details/sub-items/dto/DeleteOneParams';
 import { SpecificationDetailsSubItemsRepository } from '../../../../dal/drydock/specification-details/sub-items/SpecificationDetailsSubItemsRepository';
+import { VesselsRepository } from '../../../../dal/drydock/vessels/VesselsRepository';
 import { Command } from '../../core/cqrs/Command';
 import { UnitOfWork } from '../../core/uof/UnitOfWork';
 
 export class DeleteSubItemCommand extends Command<DeleteOneParams, void> {
     protected readonly subItemsRepo = new SpecificationDetailsSubItemsRepository();
     protected readonly uow = new UnitOfWork();
+    protected readonly tableName = 'dry_dock.specification_details_sub_item';
+    protected readonly vesselsRepository = new VesselsRepository();
 
     private params: DeleteOneParams;
 
@@ -17,6 +22,14 @@ export class DeleteSubItemCommand extends Command<DeleteOneParams, void> {
     public async MainHandlerAsync(): Promise<void> {
         await this.uow.ExecuteAsync(async (queryRunner) => {
             await this.subItemsRepo.deleteOneExistingByUid(this.params, queryRunner);
+            const vessel = await this.vesselsRepository.GetVesselBySpecification(this.params.specificationDetailsUid);
+            await SynchronizerService.dataSynchronizeManager(
+                queryRunner.manager,
+                this.tableName,
+                'uid',
+                this.params.uid,
+                vessel.VesselId,
+            );
         });
     }
 }
