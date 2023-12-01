1cd9bb3975717da3ccac553e03df88d91cb993a6
Waqar Ahmed <waqar.ahmed@jibe.solutions>
2023-12-01 17:10:47 +0500

    Removed extra Menu items

--- frontend/projects/j3-drydock/src/lib/components/specification-details/specification-details-header/specification-details-header.component.ts
+++ frontend/projects/j3-drydock/src/lib/components/specification-details/specification-details-header/specification-details-header.component.ts
@@ -39,6 +39,7 @@ export class SpecificationDetailsHeaderComponent extends UnsubscribeComponent im
     this.cd.markForCheck();
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   onValueChange(event) {
     this.saveButtonDisabled = false;
   }
