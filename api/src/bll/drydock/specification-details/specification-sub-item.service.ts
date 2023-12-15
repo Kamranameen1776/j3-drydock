b5ff09b3a6f0d0d1dde32eb87fdd02390a314e50
Abramov Oleksii <abramov.oleksii@jibe.solutions>
2023-12-15 15:26:21 +0200

    Fix for empty response

--- api/src/bll/drydock/specification-details/specification-sub-item.service.ts
+++ api/src/bll/drydock/specification-details/specification-sub-item.service.ts
@@ -9,20 +9,22 @@ export class SpecificationSubItemService {
             } as SpecificationDetailsSubItemEntity;
         });
 
-        const totalCost = data.reduce((acc, curr) => acc + (curr.cost || 0), 0);
-        const totalRow = {
-            discount: {
-                innerHTML: `<span class="totalCost">Total cost</span><style>.totalCost { font-weight: bold }</style>`,
-                cellStyle: '',
-            },
-            cost: {
-                innerHTML: `<span class="totalCost">${totalCost}</span><style>.totalCost { font-weight: bold }</style>`,
-                cellStyle: '',
-            },
-            hideActions: true,
-        } as any;
+        if (data.length > 0) {
+            const totalCost = data.reduce((acc, curr) => acc + (curr.cost || 0), 0);
+            const totalRow = {
+                discount: {
+                    innerHTML: `<span class="totalCost">Total cost</span><style>.totalCost { font-weight: bold }</style>`,
+                    cellStyle: '',
+                },
+                cost: {
+                    innerHTML: `<span class="totalCost">${totalCost}</span><style>.totalCost { font-weight: bold }</style>`,
+                    cellStyle: '',
+                },
+                hideActions: true,
+            } as any;
 
-        data.push(totalRow);
+            data.push(totalRow);
+        }
 
         return data;
     }
