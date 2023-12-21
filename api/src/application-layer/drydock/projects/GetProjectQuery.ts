5cc7e7397210a94a4187a1e81ac000e06f4db0c0
mikhail.aulov <mikhail.aulov@jibe.solutions>
2023-12-21 09:09:22 +0300

    slf validation for GetSpecificationDetailsQuery and GetProjectQuery

--- api/src/application-layer/drydock/projects/GetProjectQuery.ts
+++ api/src/application-layer/drydock/projects/GetProjectQuery.ts
@@ -2,22 +2,38 @@ import { plainToClass } from 'class-transformer';
 import { validate } from 'class-validator';
 import { Request } from 'express';
 
+import { AuthorizationException } from '../../../bll/drydock/core/exceptions';
 import { ProjectMapper } from '../../../bll/drydock/projects/ProjectMapper';
 import { ProjectsRepository } from '../../../dal/drydock/projects/ProjectsRepository';
+import { VesselsRepository } from '../../../dal/drydock/vessels/VesselsRepository';
+import { SlfAccessor } from '../../../external-services/drydock/SlfAccessor';
 import { Query } from '../core/cqrs/Query';
 import { GetProjectByUidDto } from './dtos/GetProjectByUidDto';
 import { IProjectsFromMainPageRecordDto } from './projects-for-main-page/dtos/IProjectsFromMainPageRecordDto';
 
 export class GetProjectQuery extends Query<Request, IProjectsFromMainPageRecordDto> {
     projectsRepository: ProjectsRepository;
+    slfAccessor: SlfAccessor;
+    vesselRepository: VesselsRepository;
 
     constructor() {
         super();
 
         this.projectsRepository = new ProjectsRepository();
+        this.slfAccessor = new SlfAccessor();
+        this.vesselRepository = new VesselsRepository();
     }
 
-    protected async AuthorizationHandlerAsync(): Promise<void> {
+    protected async AuthorizationHandlerAsync(request: Request): Promise<void> {
+        const token: string = request.headers.authorization as string;
+
+        const vessel = await this.vesselRepository.GetVesselByProjectUid(request.query.uid as string);
+        const assignedVessels: number[] = await this.slfAccessor.getUserAssignedVessels(token);
+
+        if (!assignedVessels.includes(vessel.VesselId)) {
+            throw new AuthorizationException(`You have no assingment on vessel: ${vessel.VesselName} `);
+        }
+
         return;
     }
 
