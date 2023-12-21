152a9bff723173ab1ebac54db00d4486d65f825e
Dmytro Parzhytskyi <parzhitsky@gmail.com>
2023-12-21 18:27:34 +0200

    [WIP]

---
+++ api/src/migration/common/1703169716977-create-specification-details-access-rights.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateSpecificationDetailsAccessRights1703169716977 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        try {
+            // …
+        } catch (error) {
+            // …
+        }
+    }
+
+    public async down(): Promise<void> {}
+}
