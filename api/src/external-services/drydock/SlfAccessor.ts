4390b021d7583dcc06bfefcb5bc114f41f4f9c8d
mikhail.aulov <mikhail.aulov@jibe.solutions>
2023-12-21 08:36:46 +0300

    GetQueryForProjects SLF

---
+++ api/src/external-services/drydock/SlfAccessor.ts
@@ -0,0 +1,12 @@
+import { ApiRequestService } from 'j2utils';
+
+import { ApiRequestMethods } from '../../shared/enum/ApiRequestMethods.enum';
+
+export class SlfAccessor {
+    UserSlfPath = 'Infra/slf/get-user-slf-details?slfType=vessel';
+
+    public async getUserAssignedVessels(token: string) {
+        const userVessels = await new ApiRequestService().infra(token, this.UserSlfPath, ApiRequestMethods.Get);
+        return userVessels.data;
+    }
+}
