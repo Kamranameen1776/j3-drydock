28a0da27d78feb9f6d95224e72b37943efe6ed08
mikhail.aulov <mikhail.aulov@jibe.solutions>
2023-12-21 09:22:39 +0300

    added type to getUserAssignedVessels

--- api/src/external-services/drydock/SlfAccessor.ts
+++ api/src/external-services/drydock/SlfAccessor.ts
@@ -5,7 +5,7 @@ import { ApiRequestMethods } from '../../shared/enum/ApiRequestMethods.enum';
 export class SlfAccessor {
     UserSlfPath = 'Infra/slf/get-user-slf-details?slfType=vessel';
 
-    public async getUserAssignedVessels(token: string) {
+    public async getUserAssignedVessels(token: string): Promise<number[]> {
         const userVessels = await new ApiRequestService().infra(token, this.UserSlfPath, ApiRequestMethods.Get);
         return userVessels.data;
     }
