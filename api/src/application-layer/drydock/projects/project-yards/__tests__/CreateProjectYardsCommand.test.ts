e918fc2e713fce5f621122bc703891f351a3011a
Ivan Chenskyi UA Dev <Ivan.Chenskyi@jibe.solutions>
2024-01-30 10:49:50 +0000

    Merged PR 91085: #658791 Update swagger endpoints
    Related work items: #658791

--- api/src/application-layer/drydock/projects/project-yards/__tests__/CreateProjectYardsCommand.test.ts
+++ api/src/application-layer/drydock/projects/project-yards/__tests__/CreateProjectYardsCommand.test.ts
@@ -1,5 +1,4 @@
-import { Request } from 'express';
-import { AccessRights, SynchronizerService } from 'j2utils'; // Import the AccessRights module
+import { SynchronizerService } from 'j2utils';
 import { QueryRunner } from 'typeorm';
 
 import * as tableName from '../../../../../common/drydock/ts-helpers/tableName';
@@ -16,7 +15,6 @@ jest.mock('j2utils');
 
 describe('CreateProjectYardsCommand', () => {
     let command: CreateProjectYardsCommand;
-    let mockRequest: Partial<Request>;
     let mockYardProjectsRepository: jest.Mocked<YardsProjectsRepository>;
     let mockUnitOfWork: jest.Mocked<UnitOfWork>;
     let mockVesselRepository: jest.Mocked<VesselsRepository>;
@@ -37,14 +35,13 @@ describe('CreateProjectYardsCommand', () => {
 
     describe('ValidationHandlerAsync', () => {
         it('should throw an error if validation fails', async () => {
-            mockRequest = {
-                body: {
-                    projectUid: 'wrongUid',
-                    yardsUids: ['wrongUid'],
-                } as CreateProjectYardsDto,
-            };
+            const mockRequestDto = {
+                projectUid: 'wrongUid',
+                yardsUids: ['wrongUid'],
+                createdBy: 'wrongUid',
+            } as CreateProjectYardsDto;
 
-            await expect(command['ValidationHandlerAsync'](mockRequest as Request)).rejects.toMatchObject([
+            await expect(command['ValidationHandlerAsync'](mockRequestDto)).rejects.toMatchObject([
                 {
                     value: 'wrongUid',
                 },
@@ -52,21 +49,18 @@ describe('CreateProjectYardsCommand', () => {
         });
 
         it('should not throw an error if validation passes', async () => {
-            mockRequest = {
-                body: {
-                    projectUid: '12963993-9397-4B5E-849E-0046FB90F564',
-                    yardsUids: ['1FE9AE21-A28A-434B-8797-E7BCFA5328EC'],
-                } as CreateProjectYardsDto,
-            };
+            const mockRequestDto = {
+                projectUid: '12963993-9397-4B5E-849E-0046FB90F564',
+                yardsUids: ['1FE9AE21-A28A-434B-8797-E7BCFA5328EC'],
+                createdBy: '12963993-9397-4B5E-849E-0046FB90F564',
+            } as CreateProjectYardsDto;
 
-            await expect(command['ValidationHandlerAsync'](mockRequest as Request)).resolves.not.toThrow();
+            await expect(command['ValidationHandlerAsync'](mockRequestDto)).resolves.not.toThrow();
         });
     });
 
     describe('MainHandlerAsync', () => {
         it('should call yardProjectsRepository.create', async () => {
-            const mockAccessRights = AccessRights as jest.Mocked<typeof AccessRights>;
-            mockAccessRights.authorizationDecode.mockReturnValue({ UserUID: 'mock-user-uid' });
             const mockSynchronizerService = SynchronizerService as jest.Mocked<typeof SynchronizerService>;
             mockSynchronizerService.dataSynchronizeByConditionManager.mockImplementation();
             const spyExecuteAsync = jest.spyOn(mockUnitOfWork, 'ExecuteAsync').mockImplementation(async (callback) => {
@@ -74,12 +68,15 @@ describe('CreateProjectYardsCommand', () => {
                 await callback(mockQueryRunner);
                 return Promise.resolve();
             });
-            mockRequest = {
-                body: new CreateProjectYardsDto(),
-            };
+            const mockRequestDto = {
+                projectUid: '12963993-9397-4B5E-849E-0046FB90F564',
+                yardsUids: ['1FE9AE21-A28A-434B-8797-E7BCFA5328EC'],
+                createdBy: '12963993-9397-4B5E-849E-0046FB90F564',
+            } as CreateProjectYardsDto;
+
             const spy = jest.spyOn(mockYardProjectsRepository, 'create').mockResolvedValue(['123']);
 
-            await command['MainHandlerAsync'](mockRequest as Request);
+            await command['MainHandlerAsync'](mockRequestDto);
 
             expect(spy).toHaveBeenCalled();
             expect(spyExecuteAsync).toHaveBeenCalled();
