82fc920a642a7d7f6b7d0e3377de26c3053271cc
Ivan Chensky <Ivan.Chenskyi@jibe.solutions>
2024-01-31 16:30:59 +0200

    add route

--- api/src/application-layer/drydock/specification-details/PMS/GetSpecificationPMSQuery.ts
+++ api/src/application-layer/drydock/specification-details/PMS/GetSpecificationPMSQuery.ts
@@ -1,12 +1,17 @@
+import { validateAgainstModel } from '../../../../common/drydock/ts-helpers/validate-against-model';
 import { SpecificationDetailsRepository } from '../../../../dal/drydock/specification-details/SpecificationDetailsRepository';
 import { Query } from '../../core/cqrs/Query';
-import { GetSpecificationPmsRequestDto } from '../dtos/GetSpecificationPMSRequestDto';
+import { GetSpecificationQueryDto } from '../dtos/GetSpecificationPMSRequestDto';
 
-export class GetSpecificationPmsQuery extends Query<GetSpecificationPmsRequestDto, Array<string>> {
+export class GetSpecificationPmsQuery extends Query<GetSpecificationQueryDto, Array<string>> {
     specificationDetailsRepository = new SpecificationDetailsRepository();
 
-    protected async MainHandlerAsync(request: GetSpecificationPmsRequestDto) {
-        const pms = await this.specificationDetailsRepository.getSpecificationPMSJobs(request.query.uid);
+    protected async ValidationHandlerAsync(request: GetSpecificationQueryDto): Promise<void> {
+        await validateAgainstModel(GetSpecificationQueryDto, request);
+    }
+
+    protected async MainHandlerAsync(request: GetSpecificationQueryDto) {
+        const pms = await this.specificationDetailsRepository.getSpecificationPMSJobs(request.uid);
         return pms.map((pmsEntity) => pmsEntity.PMSUid);
     }
 }
