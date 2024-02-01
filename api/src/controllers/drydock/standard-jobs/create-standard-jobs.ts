9be2a511d9ece746b4924ead8a499932ce65794e
Ivan Chensky <Ivan.Chenskyi@jibe.solutions>
2024-02-01 14:30:15 +0200

    add routes

--- api/src/controllers/drydock/standard-jobs/create-standard-jobs.ts
+++ api/src/controllers/drydock/standard-jobs/create-standard-jobs.ts
@@ -1,15 +1,18 @@
-import { Request, Response } from 'express';
-import { Body, Controller, Post } from 'tsoa';
+import * as express from 'express';
+import { AccessRights } from 'j2utils';
+import { Body, Controller, Post, Request, Route } from 'tsoa';
 
+import { UserFromToken } from '../../../application-layer/drydock/core/cqrs/UserDto';
 import { CreateStandardJobsCommand } from '../../../application-layer/drydock/standard-jobs';
+import { CreateStandardJobsRequestDto } from '../../../application-layer/drydock/standard-jobs/dto';
 import { StandardJobs } from '../../../entity/drydock';
 import { MiddlewareHandler } from '../core/middleware/MiddlewareHandler';
 
-async function createStandardJobs(req: Request, res: Response) {
+async function createStandardJobs(req: express.Request, res: express.Response) {
     const middlewareHandler = new MiddlewareHandler('standard_jobs');
 
-    await middlewareHandler.ExecuteAsync(req, res, async (request: Request) => {
-        const result = await new CreateStandardJobsController().createStandardJobs(request);
+    await middlewareHandler.ExecuteAsync(req, res, async (request: express.Request) => {
+        const result = await new CreateStandardJobsController().createStandardJobs(request.body, request);
 
         return result;
     });
@@ -17,13 +20,22 @@ async function createStandardJobs(req: Request, res: Response) {
 
 exports.post = createStandardJobs;
 
-// @Route('drydock/standard-jobs/create-standard-jobs')
+@Route('drydock/standard-jobs/create-standard-jobs')
 export class CreateStandardJobsController extends Controller {
     @Post()
-    public async createStandardJobs(@Body() request: Request): Promise<StandardJobs> {
+    public async createStandardJobs(
+        // TODO: check if newer version of tsoa supports this
+        // @Body() dto: CreateStandardJobsRequestDto,
+        @Body() dto: any,
+        @Request() request: express.Request,
+    ): Promise<StandardJobs> {
         const query = new CreateStandardJobsCommand();
 
-        const result = await query.ExecuteAsync(request);
+        const authUser = AccessRights.authorizationDecode(request) as UserFromToken;
+
+        dto.UserId = authUser.UserID;
+
+        const result = await query.ExecuteAsync(dto);
 
         return result;
     }
