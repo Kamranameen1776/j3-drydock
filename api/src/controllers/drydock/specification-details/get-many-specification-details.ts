13dbc757ef6c1f543be2442a525adc607c29a333
Ivan Chensky <Ivan.Chenskyi@jibe.solutions>
2024-02-02 17:48:20 +0200

    fix lint

--- api/src/controllers/drydock/specification-details/get-many-specification-details.ts
+++ api/src/controllers/drydock/specification-details/get-many-specification-details.ts
@@ -3,8 +3,8 @@ import { Body, Controller, Post, Request, Route } from 'tsoa';
 
 import { GridRequestBody } from '../../../application-layer/drydock/core/cqrs/jbGrid/GridRequestBody';
 import { GetManySpecificationDetailsQuery } from '../../../application-layer/drydock/specification-details/GetManySpecificationDetailsQuery';
-import { MiddlewareHandler } from '../core/middleware/MiddlewareHandler';
 import { Req } from '../../../common/drydock/ts-helpers/req-res';
+import { MiddlewareHandler } from '../core/middleware/MiddlewareHandler';
 
 export async function getManySpecificationDetails(req: express.Request, res: express.Response) {
     const middlewareHandler = new MiddlewareHandler();
