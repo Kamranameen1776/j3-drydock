8703aa48e7c2f01030e96f67d8fc39e0d1fff28e
Ivan Chensky <Ivan.Chenskyi@jibe.solutions>
2024-02-09 15:13:53 +0200

    add fix

--- api/src/application-layer/drydock/core/cqrs/Command.ts
+++ api/src/application-layer/drydock/core/cqrs/Command.ts
@@ -3,4 +3,7 @@ import { RequestPipeline } from './RequestPipeline';
 /**
  * Command from Command Query Responsibility Segregation pattern
  */
-export abstract class Command<TRequest extends object | void | string, TResponse> extends RequestPipeline<TRequest, TResponse> {}
+export abstract class Command<TRequest extends object | void | string, TResponse> extends RequestPipeline<
+    TRequest,
+    TResponse
+> {}
