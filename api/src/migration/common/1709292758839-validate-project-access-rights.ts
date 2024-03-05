b9e217e2e22d672e429800a794903ce013b2ccba
Neha Barayya <neha.barayya@jibe.co.in>
2024-03-05 15:14:53 +0530

    standard jobs

--- api/src/migration/common/1709292758839-validate-project-access-rights.ts
+++ api/src/migration/common/1709292758839-validate-project-access-rights.ts
@@ -19,6 +19,24 @@ export class validateProjectAccessRights1709292758839 implements MigrationInterf
             UPDATE dbo.INF_Lib_Right SET api_url = N'/infra/file/updateFileDetail' WHERE Right_Code = 'dry_dock_project_attachment_delete' AND Module_Code = 'project'
             UPDATE dbo.INF_Lib_Right SET api_url = N'/drydock/projects/delete-project' WHERE Right_Code = 'dry_dock_project_delete' AND Module_Code = 'project'
             UPDATE dbo.INF_Lib_Right SET api_url = N'/drydock/projects/update-project' WHERE Right_Code = 'dry_dock_project_immediate_closure' AND Module_Code = 'project'
+            UPDATE dbo.INF_Lib_Right SET api_url = N'/drydock/specification-details/sub-items/create-sub-item' WHERE Right_Code = 'add_sub_items' AND Module_Code = 'project'
+            UPDATE dbo.INF_Lib_Right SET api_url = N'/jms/jms_attachment/getJmsAttachmentDetails' WHERE Right_Code = 'view_attachments_section' AND Module_Code = 'project'
+            UPDATE dbo.INF_Lib_Right SET api_url = N'/drydock/specification-details/update-specification-details' WHERE Right_Code = 'edit_general_information' AND Module_Code = 'project'
+            UPDATE dbo.INF_Lib_Right SET api_url = N'/drydock/specification-details/delete-specification-details' WHERE Right_Code = 'delete_specification_detail' AND Module_Code = 'project'
+            UPDATE dbo.INF_Lib_Right SET api_url = N'/task-manager/task-manager-main/get-tm-findings-task-list' WHERE Right_Code = 'view_findings_section' AND Module_Code = 'project'
+            UPDATE dbo.INF_Lib_Right SET api_url = N'/drydock/specification-details/get-specification-details' WHERE Right_Code = 'view_specification_detail' AND Module_Code = 'project'
+            UPDATE dbo.INF_Lib_Right SET api_url = N'/drydock/specification-details/update-specification-details' WHERE Right_Code = 'edit_header_section' AND Module_Code = 'project'
+            UPDATE dbo.INF_Lib_Right SET api_url = N'/infra/file/updateFileDetail' WHERE Right_Code = 'edit_attachments' AND Module_Code = 'project'
+            UPDATE dbo.INF_Lib_Right SET api_url = N'/drydock/specification-details/sub-items/delete-sub-item' WHERE Right_Code = 'delete_sub_items' AND Module_Code = 'project'
+            UPDATE dbo.INF_Lib_Right SET api_url = N'/drydock/specification-details/sub-items/update-sub-item' WHERE Right_Code = 'edit_sub_items' AND Module_Code = 'project'
+            UPDATE dbo.INF_Lib_Right SET api_url = N'/infra/file/upload' WHERE Right_Code = 'add_attachments' AND Module_Code = 'project'
+            UPDATE dbo.INF_Lib_Right SET api_url = N'/infra/file/updateFileDetail' WHERE Right_Code = 'delete_attachments' AND Module_Code = 'project'
+            UPDATE dbo.INF_Lib_Right SET api_url = N'/drydock/specification-details/sub-items/find-sub-items' WHERE Right_Code = 'view_sub_items_section' AND Module_Code = 'project'
+            UPDATE dbo.INF_Lib_Right SET api_url = N'/drydock/specification-details/update-specification-details' WHERE Right_Code = 'view_general_information_section' AND Module_Code = 'project'
+            UPDATE dbo.INF_Lib_Right SET api_url = N'/drydock/standard-jobs/get-standard-jobs' WHERE Right_Code = 'standard_job_view_grid' AND Module_Code = 'project'
+            UPDATE dbo.INF_Lib_Right SET api_url = N'/drydock/standard-jobs/create-standard-jobs' WHERE Right_Code = 'standard_job_create' AND Module_Code = 'project'
+            UPDATE dbo.INF_Lib_Right SET api_url = N'/drydock/standard-jobs/update-standard-jobs' WHERE Right_Code = 'standard_job_edit' AND Module_Code = 'project'
+            UPDATE dbo.INF_Lib_Right SET api_url = N'/drydock/standard-jobs/delete-standard-jobs' WHERE Right_Code = 'standard_job_delete' AND Module_Code = 'project'
             `);
 
             await MigrationUtilsService.migrationLog(
@@ -26,7 +44,7 @@ export class validateProjectAccessRights1709292758839 implements MigrationInterf
                 '',
                 'S',
                 'project',
-                'validate project access rights',
+                'validate access rights',
             );
         } catch (error) {
             await MigrationUtilsService.migrationLog(
@@ -34,7 +52,7 @@ export class validateProjectAccessRights1709292758839 implements MigrationInterf
                 errorLikeToString(error),
                 'E',
                 'project',
-                'validate project access rights',
+                'validate access rights',
                 true,
             );
         }
