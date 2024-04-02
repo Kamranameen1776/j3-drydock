ef6c8ffc0252a22ab37e73b30cbfb97e40bebc3e
Prokopenko.Sergii <Prokopenko.Sergii@jibe.solutions>
2024-04-02 05:01:58 +0000

    Merged PR 98821: add tests for upsert-standard-job-popup
    Related work items: #768906

--- frontend/projects/j3-drydock/src/lib/components/standard-jobs-main/standard-jobs-main.component.spec.ts
+++ frontend/projects/j3-drydock/src/lib/components/standard-jobs-main/standard-jobs-main.component.spec.ts
@@ -40,6 +40,12 @@ describe('StandardJobsComponent', () => {
     fixture.detectChanges();
   });
 
+  afterEach(() => {
+    if (fixture.nativeElement && 'remove' in fixture.nativeElement) {
+      (fixture.nativeElement as HTMLElement).remove();
+    }
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
