156397b35f248980f03d30676224aee541e9c9c5
Zakhar.Rozental <Zakhar.Rozental@jibe.solutions>
2024-05-10 13:50:33 +0000

    Merged PR 103262: 804131 Allow editing of utilized cost in planned specification status. Fix fo...
    ## Description
    
    <!--- Please give a description of the result including relevant screen shots. -->
    
    ## Testing
    
    <!--- Please give a description of any manual testing steps that might be required. -->
    
    ## Breaking changes
    
    <!--- Does this PR cause breaking changes that might impact other developers, please describe them here and what other
    developers might have to accommodate for. -->
    
    ## Checklist
    <!---
    Please remove any checklist items not relevant to your PR
    -->
    
    - [ ] [Synchronization calls](https://dev.azure.com/jibe-erp/JiBe/_workitems/edit/685837) are added (if you added any code to modify the table)
    - [ ] This PR fulfills all acceptance criteria
    - [ ] Relevant well written tests included
    
    ## Description
    
    ![image.png](https://dev.azure.com/jibe-erp/c85e9e04-673e-4f17-8fce-3dab78037534/_apis/git/repositories/52124beb-a908-434d-bbba-8a3454873a88/pullRequests/103235/attachments/image.png)
    
    804131 Allow editing of utilized cost in planned specification status. Fix for estimated cost
    
    Cherry picked from !103235
    
    Related work items: #804131

--- api/src/shared/constants/specifications.ts
+++ api/src/shared/constants/specifications.ts
@@ -1,3 +1,3 @@
 import { TaskManagerStatus } from '../enum/task-manager-status.enum';
 
-export const DisabledSpecificationStatuses: TaskManagerStatus[] = [TaskManagerStatus.Planned, TaskManagerStatus.Closed];
+export const DisabledSpecificationStatuses: TaskManagerStatus[] = [TaskManagerStatus.Closed];
