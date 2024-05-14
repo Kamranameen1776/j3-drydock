d0e68f861543670598e058ea931be160083aa519
Prokopenko.Sergii <Prokopenko.Sergii@jibe.solutions>
2024-05-14 14:02:43 +0700

    small fix

--- frontend/projects/j3-drydock/src/lib/services/broadcast-channel.service.ts
+++ frontend/projects/j3-drydock/src/lib/services/broadcast-channel.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@angular/core';
 import { BroadcastChannels } from '../models/enums/broadcast-channels.enum';
-import { GridService } from 'jibe-components';
 
 @Injectable({ providedIn: 'root' })
 export class BroadcastChannelService {
   projectChannel = new BroadcastChannel(BroadcastChannels.ProjectsMainChannel);
 
-  constructor(private gridService: GridService) {}
+  constructor() {}
 
   postRefreshProjectsMain(isRefresh: boolean) {
     this.projectChannel.postMessage(isRefresh);
