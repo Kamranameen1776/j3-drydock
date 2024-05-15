193d39e7b2481880c874e30be3bdc89aa0e85a04
Prokopenko.Sergii <Prokopenko.Sergii@jibe.solutions>
2024-05-15 09:54:55 +0000

    Merged PR 103720: Bug 760244: Dry Dock - QC Local - Project Monitoring - Statement of Facts - F...
    Related work items: #760244

--- frontend/projects/j3-drydock/src/lib/models/enums/statement-of-fact-delete.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum eStatementOfFactsDeleteFieldNames {
-  AreYouSureYouWantToDeleteThisStatementOfFact = 'AreYouSureYouWantToDeleteThisStatementOfFact'
-}
-
-export enum eStatementOfFactsDeleteDisplayNames {
-  AreYouSureYouWantToDeleteThisStatementOfFact = 'Are you sure you want to delete this statement of fact?'
-}
